935cf8b4e3a11ee0d516ec582fdb907de4093235
Sumit dhakal <sumitdhakal082@gmail.com>
2025-12-22 21:18:41 +0545

    feat added login

--- src/handlers/auth.js
+++ src/handlers/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register } from "../services/auth.js";
+import { register, login } from "../services/auth.js";
 
 const AUTH_ROUTER = Router();
 
@@ -10,6 +10,15 @@ AUTH_ROUTER.post("/register", async (req, res, next) => {
     res.status(200).json(result);
   } catch (error) {
     next(error);
+  }
+});
+
+AUTH_ROUTER.post("/login", async (req, res, next) => {
+  try {
+    const result = await login(req.body);
+    res.status(200).json(result);
+  } catch (error) {
+    next(erroer);
   }
 });
 
