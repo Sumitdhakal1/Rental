3f67da986e3178e90f5fd09b49f07577a912bcf4
Sumit dhakal <sumitdhakal082@gmail.com>
2025-12-22 21:39:35 +0545

    feat added post method

--- src/handlers/rentSpace.js
+++ src/handlers/rentSpace.js
@@ -1,12 +1,30 @@
 import { Router } from "express";
-import RentSpace from "../models/rentSpace";
+import { getAllRentSpace, createRentSpace } from "../services/rentSpace.js";
+import { authMiddleware, isOwner } from "../middleware/auth.js";
 
-RENTSPACE_ROUTER = Router();
+const RENTSPACE_ROUTER = Router();
 
-RENTSPACE_ROUTER.get("/", async (req, res, next) => {
+RENTSPACE_ROUTER.get("/home", async (req, res, next) => {
   try {
-    const rentSpce = await RentSpace();
+    const rentSpce = await getAllRentSpace(req.query);
+    res.status(200).json(rentSpce);
   } catch (error) {
     next(error);
   }
 });
+
+RENTSPACE_ROUTER.post(
+  "/create",
+  authMiddleware,
+  isOwner,
+  async (req, res, next) => {
+    try {
+      const rentSpace = await createRentSpace(req.body, req.user);
+      res.status(200).json(rentSpace);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default RENTSPACE_ROUTER;
